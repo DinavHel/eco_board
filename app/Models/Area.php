fb576e5231f62cd512bed1e11d9f3afa3d88d9e4
Adrián González Gómez <gonzalezgomez.adriangg@gmail.com>
2024-10-12 21:03:33 +0200

    feat: Engadir relacion Area ten moitos Sensores

--- app/Models/Area.php
+++ app/Models/Area.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Area extends Model
@@ -14,5 +15,10 @@ class Area extends Model
     public function facility(): BelongsTo
     {
         return $this->belongsTo(Facility::class);
+    }
+
+    public function sensors(): HasMany
+    {
+        return $this->hasMany(Sensor::class);
     }
 }
